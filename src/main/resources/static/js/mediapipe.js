a3675f9e5a0a9f06c6630b2bed3702e1d5eb05b7
Hongsugyeong <olivia258@naver.com>
2023-03-03 16:41:21 +0900

    feat : mediapipe segmentmask 제거

--- src/main/resources/static/js/mediapipe.js
+++ src/main/resources/static/js/mediapipe.js
@@ -1,4 +1,3 @@
-
 const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
@@ -6,6 +5,10 @@ const landmarkContainer = document.getElementsByClassName('landmark-grid-contain
 const grid = new LandmarkGrid(landmarkContainer);
 
 function onResults(results) {
+  console.log(results.poseWorldLandmarks);
+  //result : pose_landmarks / pose_world_landmarks
+  //pose_landmarks : 포즈 랜드마크 목록
+  //pose_world_landmarks : 세계 좌표의 또 다른 포즈 랜드마크 목록
   if (!results.poseLandmarks) {
     grid.updateLandmarks([]);
     return;
@@ -13,19 +16,19 @@ function onResults(results) {
 
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(results.segmentationMask, 0, 0,
-                      canvasElement.width, canvasElement.height);
 
-  // Only overwrite existing pixels.
+  //globalCompositeOperation : 캔버스에 그리려는 색이 그려져 있던 색과 겹쳐질 때 어떤식으로 조합할지 지정
+  // source-in : 대상값과 겹쳐진 부분만; 즉 나중에 그려진 도형의 겹쳐지는 부분만 표시됨
   canvasCtx.globalCompositeOperation = 'source-in';
-  canvasCtx.fillStyle = '#00FF00';
   canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
 
-  // Only overwrite missing pixels.
+  // destination-atop : 대상 그림과 겹쳐진 부분만; 즉 나중에 그려진 도형 영역만 표시
+  // -> 겹쳐진 부분은 처음 그려진 도형이 표시
   canvasCtx.globalCompositeOperation = 'destination-atop';
   canvasCtx.drawImage(
       results.image, 0, 0, canvasElement.width, canvasElement.height);
 
+  // source-over : 기본값, 대상값이 위로; 즉 처음 그려진 도형 위에 나중에 그려진 도형이 표시 됨
   canvasCtx.globalCompositeOperation = 'source-over';
   drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
                  {color: '#00FF00', lineWidth: 4});
@@ -40,12 +43,12 @@ const pose = new Pose({locateFile: (file) => {
   return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
 }});
 pose.setOptions({
-  modelComplexity: 1,
+  modelComplexity: 1, //모델 복잡성 기본값:1
   smoothLandmarks: true,
-  enableSegmentation: true,
-  smoothSegmentation: true,
-  minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5
+  enableSegmentation: true, //포즈 랜드마크 외에도 솔루션 분할 마스크 생성
+  smoothSegmentation: true, // enableSegmentation false이면 적용 X
+  minDetectionConfidence: 0.5, //감지에 대한 사람 감지 모델의 최소 신뢰 값
+  minTrackingConfidence: 0.5 //랜드마크 추적 모델의 최소 신뢰 값
 });
 pose.onResults(onResults);
 
